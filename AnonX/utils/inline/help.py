382792ce0591b7a71ba879e7a4cb5de972f77f02
Dev-kargali <137890309+Dev-kargali@users.noreply.github.com>
2023-06-27 14:22:44 +0200

    Add files via upload

---
+++ AnonX/utils/inline/help.py
@@ -0,0 +1,112 @@
+from typing import Union
+
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+def help_pannel(_, START: Union[bool, int] = None):
+    first = [
+        InlineKeyboardButton(
+            text=_["CLOSEMENU_BUTTON"], callback_data=f"close"
+        )
+    ]
+    second = [
+        InlineKeyboardButton(
+            text=_["BACK_BUTTON"],
+            callback_data=f"settingsback_helper",
+        ),
+        InlineKeyboardButton(
+            text=_["CLOSEMENU_BUTTON"], callback_data=f"close"
+        ),
+    ]
+    mark = second if START else first
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text="[𖢿اوامر الادمن𖢿]",
+                    callback_data="help_callback hb1",
+                ),
+                InlineKeyboardButton(
+                    text="[𖢿المصادقه𖢿]",
+                    callback_data="help_callback hb2",
+                ),
+                InlineKeyboardButton(
+                    text="[𖢿القائمه السوداء𖢿]",
+                    callback_data="help_callback hb3",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text="[𖢿الاذاعه𖢿]",
+                    callback_data="help_callback hb4",
+                ),
+                InlineKeyboardButton(
+                    text="[𖢿المحظورين𖢿]",
+                    callback_data="help_callback hb12",
+                ),
+                InlineKeyboardButton(
+                    text="[𖢿كلمات𖢿]",
+                    callback_data="help_callback hb5",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text="[𖢿بينج𖢿]",
+                    callback_data="help_callback hb7",
+                ),
+                InlineKeyboardButton(
+                    text="[𖢿تشغيل𖢿]",
+                    callback_data="help_callback hb8",
+                ),
+                InlineKeyboardButton(
+                    text="[𖢿قائمة التشغيل𖢿]",
+                    callback_data="help_callback hb6",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text="[𖢿المكالمات الصوتيه𖢿]",
+                    callback_data="help_callback hb10",
+                ),
+                InlineKeyboardButton(
+                    text="[𖢿ستارت𖢿]",
+                    callback_data="help_callback hb11",
+                ),
+                InlineKeyboardButton(
+                    text="[𖢿اوامر المطورين𖢿]",
+                    callback_data="help_callback hb9",
+                ),
+            ],
+            mark,
+        ]
+    )
+    return upl
+
+
+def help_back_markup(_):
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=_["BACK_BUTTON"],
+                    callback_data=f"settings_back_helper",
+                ),
+                InlineKeyboardButton(
+                    text=_["CLOSE_BUTTON"], callback_data=f"close"
+                )
+            ]
+        ]
+    )
+    return upl
+
+
+def private_help_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="❄ مساعده ❄",
+                callback_data="settings_back_helper",
+            ),
+        ],
+    ]
+    return buttons
---
+++ AnonX/utils/inline/start.py
@@ -0,0 +1,58 @@
+from typing import Union
+
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+import config
+
+
+
+def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="ضيفني",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="**الـاوامر**",
+                callback_data="settings_back_helper",
+            ),
+            InlineKeyboardButton(
+                text="المساعدة", callback_data="settings_helper"
+            ),
+        ],
+     ]
+    return buttons
+
+
+def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="ضيفني",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="**الـاوامر**", callback_data="settings_back_helper"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="لتنصيب بوت", url=f"https://t.me/bp_bp"
+            ),
+            InlineKeyboardButton(
+                text="👤 مطور البوت", user_id=OWNER
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="𓏺𝙎𝙊𝙐𝙍𝘾𝞝 𝙃𝘼𝙔𝘼", url=f"https://t.me/HL_BG"
+            )
+        ],
+      
+     ]
+    return buttons
