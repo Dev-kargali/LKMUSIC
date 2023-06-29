f2521a89ff183de232793cf730cfc4d1c956d5ee
Dev-kargali <137890309+Dev-kargali@users.noreply.github.com>
2023-06-29 18:47:23 +0300

    Update settings.py

--- AnonX/utils/inline/settings.py
+++ AnonX/utils/inline/settings.py
@@ -24,7 +24,7 @@ def setting_markup(_):
         ],
         [
             InlineKeyboardButton(
-                text="• الدعم •", url=f"https://t.me/HL_BG",
+                text="• الدعم •", url=f"https://t.me/L_B_K1",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
